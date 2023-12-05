a12e75b0b196732c84cc27e4f80018d0b1dddee1
NikkitaTorres <nikkitatorres@yahoo.com>
2023-12-05 08:40:33 -0800

    creates a help queue via ticket systems

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewTicketForm(props){
+  return (
+    <React.Fragment>
+      <h3>THis is a form.</h3>
+    </React.Fragment>
+  );
+}
+
+export default NewTicketForm;
