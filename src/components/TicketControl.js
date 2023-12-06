7d55984d4ca8061db3a72784f15972f2a4e7c425
NikkitaTorres <nikkitatorres@yahoo.com>
2023-12-05 18:08:27 -0800

    undo previous changes

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,67 +7,24 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
-      currentPage: 1
+      formVisibleOnPage: false
     };
   }
 
-  handlePageChange = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1
-    }));
-  }
-
-  handleClick = () => {
-    if (this.state.currentPage < 3) {
-      this.handlePageChange();
-    } else if (this.state.currentPage === 3) {
-      // Add additional conditions if needed before moving to the next page or showing the form
-      this.handlePageChange();
-    } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage
-      }));
-    }
-  }
-
-  render() {
+  render(){
     let currentlyVisibleState = null;
-    let buttonText = null;
-
-    if (this.state.currentPage === 1) {
-      currentlyVisibleState = (
-        <div>
-          <p>Have you gone through all the steps on the Learn How to Program debugging lesson?</p>
-        </div>
-      );
-    } else if (this.state.currentPage === 2) {
-      currentlyVisibleState = (
-        <div>
-          <p>Have you asked another pair for help?</p>
-        </div>
-      );
-    } else if (this.state.currentPage === 3) {
-      currentlyVisibleState = (
-        <div>
-          <p>Have you spent 15 minutes going through the problem documenting every step?</p>
-        </div>
-      );
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
     } else {
-      currentlyVisibleState = <TicketList />;
-      buttonText = "Add Ticket";
+      currentlyVisibleState = <TicketList />
     }
-
     return (
       <React.Fragment>
         {currentlyVisibleState}
-        {this.state.currentPage <= 3 && (
-          <button onClick={this.handleClick}>Next</button>
-        )}
-        {this.state.currentPage === 4 && <button onClick={this.handleClick}>{buttonText}</button>}
       </React.Fragment>
     );
   }
+
 }
 
 export default TicketControl;
